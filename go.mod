8bde6081696cb3743e5eb091e79549d3a6e01c81
Yusuf Yaman <nxjoseph@protonmail.com>
2024-06-12 18:50:53 +0300

    initial commit but not finished

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/nxjosephofficial/automated-ytdlp-downloader
+
+go 1.22.2
---
+++ main.go
@@ -0,0 +1,73 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"os/exec"
+	"strings"
+)
+
+func main() {
+
+	ytdlpPath, err := check_ytdlp()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	link, err := getLink()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	ytdlpArgs, err := getArgs()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	downloadLink(ytdlpPath, ytdlpArgs, link)
+}
+
+func check_ytdlp() (string, error) {
+	path, err := exec.LookPath("yt-dlp")
+	if err != nil {
+		return "", err
+	}
+	return path, nil
+}
+
+func getLink() (string, error) {
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Print("Enter link: ")
+	link, err := reader.ReadString('\n')
+	if err != nil {
+		return "", err
+	}
+	return strings.TrimSpace(link), nil
+}
+
+func downloadLink(ytdlpPath string, ytdlpArgs []string, link string) (string, error) {
+	cmd := exec.Command(ytdlpPath, append(ytdlpArgs, link)...)
+	output, err := cmd.CombinedOutput()
+	if err != nil {
+		return "", nil
+	}
+	return string(output), nil
+}
+
+func getArgs() ([]string, error) {
+	var ytdlpArgs []string
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Print("Choose type: 1) Audio\t2) Video\n")
+	contentType, err := reader.ReadString('\n')
+	if err != nil {
+		return nil, err
+	}
+	if strings.TrimSpace(contentType) == "1" {
+		ytdlpArgs = []string{"-x", "--audio-format", "mp3"}
+	} else {
+		ytdlpArgs = []string{""}
+	}
+	return ytdlpArgs, nil
+}
